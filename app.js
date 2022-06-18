ca71090aefb1276d26ce452eafee2ff466cba171
Vinícius Gonçalves <oldz.registros@gmail.com>
2022-06-18 16:59:06 -0300

    Code refactoring

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
+const bodyTag = document.getElementsByTagName('body')[0]
 const label = document.querySelector('label')
 const uil = document.querySelector('.uil')
+const inputWrapper = document.querySelector('.input-wrapper')
 
-const maxLength = input.maxLength
+const changeColor = (element, key, color) => {
+    return element.style[key] = color
+}
 
-input.addEventListener('input', () => {
+username.addEventListener('input', () => {
 
-    label.textContent = input.maxLength - input.value.length
+    const usernameInputValueLength = username.value.length
+    const minCharactersLength = username.min
+    const maxCharactersLength = username.maxLength
+    const restHowMany = maxCharactersLength - usernameInputValueLength
 
-    if(input.value.length < input.min) {
-        console.log('É necessário 6 dígitos')
-        document.getElementsByTagName('body')[0].style.backgroundColor = '#470c0a'
-        label.style.color = 'red'
-        uil.style.color = '#470c0a'
-    }else {
-        console.log('Sucesso')
-        document.getElementsByTagName('body')[0].style.backgroundColor = '#15a066'
-        label.style.color = '#15a066'
-        uil.style.color = '#15a066'
+    
+    const dynamicResult = restHowMany === 0 ? 'ok' : restHowMany
+    label.textContent = dynamicResult
+    
+    if(usernameInputValueLength < minCharactersLength) {
+        changeColor(bodyTag, 'backgroundColor', '#470c0a')
+        changeColor(label, 'color',  'red')
+        changeColor(label, 'color', '#470c0a')
+        changeColor(uil, 'color', '#470c0a')
 
+        return
     }
+
+    changeColor(bodyTag, 'backgroundColor', '#15a066')
+    changeColor(label, 'color', '#15a066')
+    changeColor(uil, 'color', '#15a066')
+    
 }) 
 
-label.textContent = maxLength
